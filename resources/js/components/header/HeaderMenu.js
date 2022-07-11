bd0885a20638908f6dd0640a7cfaefe7f021d1e6
Nguyen Dang Hieu <danghieu140701@gmail.com>
2022-07-11 23:55:15 +0700

    fear(fe):show password. Complete assignment without cart

--- resources/js/components/header/HeaderMenu.js
+++ resources/js/components/header/HeaderMenu.js
@@ -3,12 +3,12 @@ import "../../../css/app.css";
 import { Alert, Navbar } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
 import { Modal, Button } from "react-bootstrap";
-import { InputGroup, FormControl } from "react-bootstrap";
+import { InputGroup, FormControl, Form } from "react-bootstrap";
 import { FcPortraitMode, FcKey } from "react-icons/fc";
 
 import axios from "axios";
 import { Dropdown, Toggle } from "react-bootstrap";
-import Swal from 'sweetalert2/dist/sweetalert2.js'
+import Swal from "sweetalert2/dist/sweetalert2.js";
 
 class HeaderMenu extends React.Component {
   constructor(props) {
@@ -20,10 +20,23 @@ class HeaderMenu extends React.Component {
       first_name: "",
       last_name: "",
       message: "",
+      hidden: true,
     };
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  toggleShow() {
+    this.setState({ hidden: !this.state.hidden });
   }
   componentDidMount() {
     this.getName();
+    if (this.props.password) {
+      this.setState({ password: this.props.password });
+    }
   }
 
   openModal = () => {
@@ -40,7 +53,6 @@ class HeaderMenu extends React.Component {
     const data = {
       email: this.state.email,
       password: this.state.password,
-      
     };
     const config = {
       headers: {
@@ -54,18 +66,18 @@ class HeaderMenu extends React.Component {
         console.log(res.data);
         localStorage.setItem("token", res.data.token);
         Swal.fire({
-          icon: 'success',
-          title: 'Success',
+          icon: "success",
+          title: "Success",
           toast: true,
-          position: 'top-right',
-          iconColor: 'white',
+          position: "top-right",
+          iconColor: "white",
           customClass: {
-            popup: 'colored-toast'
+            popup: "colored-toast",
           },
           showConfirmButton: false,
           timer: 2500,
-          timerProgressBar: true
-        })
+          timerProgressBar: true,
+        });
 
         this.setState({
           email: "",
@@ -73,21 +85,19 @@ class HeaderMenu extends React.Component {
         });
       })
       .catch((err) => {
-    
         Swal.fire({
-          icon: 'error',
+          icon: "error",
           title: err.response.data.message,
           toast: true,
-          position: 'top-right',
-          iconColor: 'white',
+          position: "top-right",
+          iconColor: "white",
           customClass: {
-            popup: 'colored-toast'
+            popup: "colored-toast",
           },
           showConfirmButton: false,
           timer: 2500,
-          timerProgressBar: true
-        })
-
+          timerProgressBar: true,
+        });
       });
     console.log("localStorage", localStorage.getItem("token"));
     this.getName();
@@ -105,34 +115,32 @@ class HeaderMenu extends React.Component {
         first_name: res.data.first_name,
         last_name: res.data.last_name,
       });
-    
-
     });
   };
 
   logOut() {
     const config = {
       headers: {
-        Authorization: "Bearer " + localStorage.getItem("token").split('|')[1],
+        Authorization: "Bearer " + localStorage.getItem("token").split("|")[1],
       },
     };
 
     console.log(config);
-    axios.get("http://localhost:8000/api/logout", config).then((res) => {
-      console.log(res);
-      localStorage.removeItem("token");
-      window.location.reload();
-    })
-  .catch((err) => {
-    console.log(err);
-  });
-
+    axios
+      .get("http://localhost:8000/api/logout", config)
+      .then((res) => {
+        console.log(res);
+        localStorage.removeItem("token");
+        window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
     return (
       <>
-       
         <Navbar className="nav-link" id="nav-link">
           <Navbar.Brand>
             <NavLink
@@ -148,7 +156,6 @@ class HeaderMenu extends React.Component {
               to="/ShopPage"
               className={({ isActive }) => (isActive ? "link-active" : "link")}
             >
-            
               Shop
             </NavLink>
           </Navbar.Brand>
@@ -184,7 +191,12 @@ class HeaderMenu extends React.Component {
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu className="logout">
-                  <Dropdown.Item  className="log-out-btn" onSelect={()=>this.logOut()}>Log out</Dropdown.Item>
+                  <Dropdown.Item
+                    className="log-out-btn"
+                    onSelect={() => this.logOut()}
+                  >
+                    Log out
+                  </Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
             </Navbar.Brand>
@@ -195,26 +207,24 @@ class HeaderMenu extends React.Component {
           size="lg"
           aria-labelledby="contained-modal-title-vcenter"
           centered
-          
         >
-          <Modal.Header className="modal-login" >
+          <Modal.Header className="modal-login">
             <Modal.Title id="contained-modal-title-vcenter">Login</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <InputGroup className="mb-2 "  >
+            <InputGroup className="mb-2 ">
               <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroup-sizing-default" >
+                <InputGroup.Text id="inputGroup-sizing-default">
                   <div>
                     {" "}
                     <FcPortraitMode /> Email{" "}
                   </div>
                 </InputGroup.Text>
               </InputGroup.Prepend>
-              <FormControl 
+              <FormControl
                 aria-label="Default"
                 aria-describedby="inputGroup-sizing-default"
                 onChange={(e) => this.setState({ email: e.target.value })}
-                
               />
             </InputGroup>
             <InputGroup className="mb-2 password-modal">
@@ -223,14 +233,15 @@ class HeaderMenu extends React.Component {
                   <FcKey /> Password
                 </InputGroup.Text>
               </InputGroup.Prepend>
-              <FormControl  type="password"
-                aria-label="Default"
-                aria-describedby="inputGroup-sizing-default"
+              <FormControl
+                type={this.state.hidden ? "password" : "text"}
                 onChange={(e) => this.setState({ password: e.target.value })}
-             
               />
             </InputGroup>
-           
+            {/* <button onClick={this.toggleShow}>Show</button> */}
+            <Form.Group controlId="formBasicCheckbox" onClick={this.toggleShow}>
+              <Form.Check type="checkbox" label="Show password" />
+            </Form.Group>
           </Modal.Body>
           <Modal.Footer>
             <Button onClick={this.submit} className="btn-login">
